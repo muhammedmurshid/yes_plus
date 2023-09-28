f7655b3b144c6167f1c0df266ac493e4debaee85
Murshid <muhsink00000@gmail.com>
2023-09-28 16:25:50 +0530

    default attendance is present

--- models/attendance.py
+++ models/attendance.py
@@ -7,11 +7,11 @@ class YesPlusAttendanceForFiveDays(models.Model):
     _name = 'yes_plus.attendance'
 
     student_name = fields.Char('Student Name')
-    day_one = fields.Boolean('Day 1')
-    day_two = fields.Boolean('Day 2')
-    day_three = fields.Boolean('Day 3')
-    day_four = fields.Selection([('full_day', 'Full Day'), ('half_day', 'Half Day'), ('absent', 'Absent')], 'Day 4')
-    day_five = fields.Selection([('full_day', 'Full Day'), ('half_day', 'Half Day'), ('absent', 'Absent')], 'Day 5')
+    day_one = fields.Boolean('Day 1', default=True)
+    day_two = fields.Boolean('Day 2', default=True)
+    day_three = fields.Boolean('Day 3', default=True)
+    day_four = fields.Selection([('full_day', 'Full Day'), ('half_day', 'Half Day'), ('absent', 'Absent')], 'Day 4', default='full_day')
+    day_five = fields.Selection([('full_day', 'Full Day'), ('half_day', 'Half Day'), ('absent', 'Absent')], 'Day 5', default='full_day')
     yes_plus_attendance_id = fields.Many2one('yes_plus.logic')
     date = fields.Date('Date')
     student_id = fields.Integer()
